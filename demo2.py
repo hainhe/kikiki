93f08bfba06f7db1768d84d794ff5c4daa96e9a9
hainhe <124561362+hainhe@users.noreply.github.com>
2025-02-16 11:51:58 +0700

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -26,11 +26,11 @@ def webhook():
         return jsonify({"error": "Failed to read data", "details": str(e)}), 400
     
     # Bot1 chỉ gửi tín hiệu LONG/SHORT
-    if "🚀" in alert_message:
+    if "LONG" in alert_message:
         send_telegram_message(BOT1_TOKEN, CHAT_ID, alert_message)
     
     # Bot2 chỉ gửi tín hiệu theo dõi nến
-    if "📉" in alert_message:
+    if "SHORT" in alert_message:
         send_telegram_message(BOT2_TOKEN, CHAT_ID, alert_message)
     
     return jsonify({"status": "ok"})
