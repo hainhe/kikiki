6a3640b7fc028a950a49730539f9b18eb36e8886
hainhe <124561362+hainhe@users.noreply.github.com>
2025-02-17 23:30:48 +0700

    Update and rename build.sh to dockerfile

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Cài đặt Chrome trên môi trường Render
-echo "Installing Google Chrome..."
-apt-get update
-apt-get install -y wget
-wget -qO- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /usr/share/keyrings/google-chrome-keyring.gpg
-echo 'deb [signed-by=/usr/share/keyrings/google-chrome-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
-apt-get update
-apt-get install -y google-chrome-stable
